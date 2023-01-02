ffed25490705b7841425b828a4a059a641934fea
mostfa99 <43701126+mostfa99@users.noreply.github.com>
2023-01-02 04:46:37 +0200

    Authentication Basics v1

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -36,12 +36,14 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            // password confirmation
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            // do hash to password
             'password' => Hash::make($request->password),
         ]);
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\CatagoriesController;
+use App\Http\Controllers\Admin\ProductsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,5 +22,16 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/admin/catagories',[CatagoriesController::class,'index'])->name('catagories.index');
+Route::get('/admin/catagories/create',[CatagoriesController::class,'create'])->name('catagories.create');
+Route::post('/admin/catagories',[CatagoriesController::class,'store'])->name('catagories.store'); //to edit in database we will use post or delete but cant use get
+Route::get('admin/catagories/{id}',[CatagoriesController::class, 'show'])->name('catagories.show');
+Route::get('admin/catagories/edit/{id}',[CatagoriesController::class, 'edit'])->name('catagories.edit');
+Route::put('admin/catagories/{id}',[CatagoriesController::class, 'update'])->name('catagories.update');
+Route::delete('admin/catagories/{id}',[CatagoriesController::class, 'destroy'])->name('catagories.destroy');
+
+// Route::resource('/admin/catagories', CatagoriesController::class);
+Route::resource('/admin/products', ProductsController::class)->middleware('auth');
 
 require __DIR__.'/auth.php';
